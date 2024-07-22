c750a5d253a24627ab433b7d99ad5619323c72a3
Pecoden <www.zenap35@gmail.com>
2024-07-22 15:52:47 +0300

    write cv

--- Layout/Arabic/js/write.js
+++ Layout/Arabic/js/write.js
@@ -1,58 +1,155 @@
-
-function toggleDivs() {
-    var firstDiv = document.getElementById('firstDiv');
-    var secondDiv = document.getElementById('secondDiv');
-    var basebtn = document.getElementById('basebtn');
-
-    if (firstDiv.style.display !== 'none') {
-        firstDiv.style.display = 'none';
-        basebtn.style.display = 'none';
-        secondDiv.style.display = 'block';
-    } else {
-        firstDiv.style.display = 'block';
-        basebtn.style.display = 'block';
-        secondDiv.style.display = 'none';
-    }
+var basebtn = document.getElementById('basebtn');
+var save = document.getElementById('save');
+for (var i = 1; i <= 14; i++) {
+    divs.push(document.getElementById('Div' + i));
 }
 
+
+
+
+function toggleDivs0() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div2.style.display = 'block';
+        Div3.style.display = 'none';
+        Div4.style.display = 'none';
+        Div5.style.display = 'none';
+        Div6.style.display = 'none';
+        Div7.style.display = 'none';
+        Div8.style.display = 'none';
+        Div9.style.display = 'none';
+        Div10.style.display = 'none';
+        Div11.style.display = 'none';
+        Div12.style.display = 'none';
+        Div13.style.display = 'none';
+        Div14.style.display = 'none';
+
+    } else {
+        basebtn.style.display = 'table';
+        save.style.display = 'none';
+        Div1.style.display = 'block';
+        Div2.style.display = 'none';
+        Div3.style.display = 'none';
+        Div4.style.display = 'none';
+        Div5.style.display = 'none';
+        Div6.style.display = 'none';
+        Div7.style.display = 'none';
+        Div8.style.display = 'none';
+        Div9.style.display = 'none';
+        Div10.style.display = 'none';
+        Div11.style.display = 'none';
+        Div12.style.display = 'none';
+        Div13.style.display = 'none';
+        Div14.style.display = 'none';
+    }
+}
 
 function toggleDivs1() {
-    var firstDiv = document.getElementById('firstDiv');
-    var thirdDiv = document.getElementById('thirdDiv');
-    var basebtn = document.getElementById('basebtn');
-
-    if (firstDiv.style.display !== 'none') {
-        firstDiv.style.display = 'none';
+    if (Div1.style.display !== 'none') {
         basebtn.style.display = 'none';
-        thirdDiv.style.display = 'block';
-    } else {
-        firstDiv.style.display = 'block';
-        basebtn.style.display = 'block';
-        thirdDiv.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div3.style.display = 'block';
     }
 }
 
+function toggleDivs2() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div4.style.display = 'block';
+    }
+}
 
+function toggleDivs3() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div5.style.display = 'block';
+    }
+}
 
+function toggleDivs4() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div6.style.display = 'block';
+    }
+}
 
+function toggleDivs5() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div7.style.display = 'block';
+    }
+}
 
+function toggleDivs6() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div8.style.display = 'block';
+    }
+}
 
+function toggleDivs7() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div9.style.display = 'block';
+    }
+}
 
+function toggleDivs8() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div10.style.display = 'block';
+    }
+}
 
+function toggleDivs9() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div11.style.display = 'block';
+    }
+}
 
+function toggleDivs10() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div12.style.display = 'block';
+    }
+}
 
+function toggleDivs11() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div13.style.display = 'block';
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+function toggleDivs12() {
+    if (Div1.style.display !== 'none') {
+        basebtn.style.display = 'none';
+        save.style.display = 'flex';
+        Div1.style.display = 'none';
+        Div14.style.display = 'block';
+    }
+}
